a5b06650f693fcd1b54f0f9e30c9372548e570d5
Diego A. Perez Gandarillas <diego.perez16@techexchange.in>
2025-03-09 04:54:45 +0000

    Rebased from main and resolved merging conflicts

--- modules.py
+++ modules.py
@@ -12,11 +12,6 @@ from internals import create_component
 from data_fetcher import get_user_workouts, get_user_posts, users
 from PIL import Image
 import pandas as pd
-
-
-#add your tab to the list when you're ready, https://docs.streamlit.io/develop/api-reference/layout/st.tabs
-#recent_workouts_tab, post_tab, gen_ai_advice_tab = sl.tabs(['Recent Workouts', 'Posts', 'Gen AI Advice'])
- 
 # This one has been written for you as an example. You may change it as wanted.
 def display_my_custom_component(value):
     """Displays a 'my custom component' which showcases an example of how custom
@@ -181,7 +176,8 @@ def display_recent_workouts(workouts_list):
         Outputs a table to website
     """
     #Made with slight debugging help from Gemini: https://g.co/gemini/share/d246196d413a
-    #with recent_workouts_tab:
+    import pandas as pd
+    
     sl.title('Recent Workouts') 
     if len(workouts_list) == 0:
         sl.subheader("No Workout Data To Display")
